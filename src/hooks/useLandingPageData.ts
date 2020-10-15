5bf9babce5d29cdbb09105518499580ece18bfc8
Douglas <douglasrdsousa1@gmail.com>
2020-10-15 16:44:20 -0300

    feat: added simple localstorage logic to LP's

--- src/hooks/useLandingPageData.ts
+++ src/hooks/useLandingPageData.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { initFirebase } from '@/lib/Firebase';
 import { CTA_Props } from '@/components/CTA';
 
@@ -17,20 +17,47 @@ const DUMMY_DATA: LandingPageResponse = {
 }
 
 export function useLandingPageData({ page }: Props): LandingPageResponse {
-  const [landingPageData, setLandingPageDate] = useState<LandingPageResponse>(DUMMY_DATA);
+  const getData = useCallback((): LandingPageResponse => {
+    if(typeof window !== 'undefined') {
+      const data = localStorage.getItem('lp-data');
+      if (!data) return DUMMY_DATA;
+      const parsedData = JSON.parse(data);
+      console.log(parsedData);
+      if (!(parsedData.timestamp + Number(process.env.GATSBY_FIREBASE_CACHE) > new Date().valueOf())) {
+        return DUMMY_DATA;
+      }
+      delete parsedData.timestamp;
+      return parsedData;
+    }
+    return DUMMY_DATA;
+  }, []);
+
+  const storeData = useCallback((json: string): void => {
+    if (typeof window !== 'undefined') {
+      const parsedData = JSON.parse(json);
+      parsedData.timestamp = new Date().valueOf();
+      localStorage.setItem('lp-data', JSON.stringify(parsedData));
+    }
+  }, []);
+
+  const [landingPageData, setLandingPageDate] = useState<LandingPageResponse>(getData());
 
   useEffect(() => {
     const remoteConfig = initFirebase().remoteConfig();
 
     remoteConfig.settings = {
       fetchTimeoutMillis: 60000,
-      minimumFetchIntervalMillis: Number(process.env.GATSBY_FIREBASE_CACHE_EXPIRATION)
+      minimumFetchIntervalMillis: Number(process.env.GATSBY_FIREBASE_CACHE),
     };
+
+    console.log(process.env.GATSBY_FIREBASE_CACHE);
 
     remoteConfig.fetchAndActivate()
       .then((isNewFetch) => {
         console.log(isNewFetch ? 'Novos dados' : 'Dados do cache');
-        setLandingPageDate(JSON.parse(remoteConfig.getString(page)))
+        const data = remoteConfig.getString(page);
+        setLandingPageDate(JSON.parse(data));
+        storeData(data);
       })
       .catch((error) => console.log('Erro ao atualizar remote config', error));
   }, []);
